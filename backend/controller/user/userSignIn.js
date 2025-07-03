4665f137a0c52979928569a8e7b132af2920bd98
abhiyadav-123 <ay005562@gamil.com>
2025-07-03 21:31:54 +0530

    user login out

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -45,9 +45,10 @@ async function userSignInController(req, res) {
             });
 
             const tokenOption = {
-                httpOnly: true,
-                secure: true,
-            };
+            httpOnly : true,
+            secure : true,
+            sameSite : 'None'
+        };
 
             res
                 .cookie("token", token, tokenOption)
